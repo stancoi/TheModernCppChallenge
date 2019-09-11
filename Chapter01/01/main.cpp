a6acdf745289509a2597bd66dbd5294880a7ea2f
stanco <stanco81@gmail.com>
2019-09-11 10:49:08 +0200

    Update main

--- Chapter01/01/main.cpp
+++ Chapter01/01/main.cpp
@@ -2,5 +2,17 @@
 
 int main()
 {
+    unsigned long long sum = 0;
+    unsigned long limit;
+    std::cout << "Enter limit: ";
+    std::cin >> limit;
+    for (unsigned int i = 3; i < limit; ++i)
+    {
+        if ((i % 3 == 0) || (i % 5 == 0))
+        {
+            sum += i;
+        }
+    }
+    std::cout << "Sum: " << sum << '\n';
     return 0;
 }
