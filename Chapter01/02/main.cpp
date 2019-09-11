5ec53afaf816745e00d6e390652484de990b53c5
stanco <stanco81@gmail.com>
2019-09-11 11:23:51 +0200

    Greatest common divisor

---
+++ Chapter01/02/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+unsigned int gcd_euclid(unsigned int const a, unsigned int const b)
+{
+    return b == 0 ? a : gcd_euclid(b, a % b);
+}
+
+unsigned int gcd_euclid2 (unsigned int const a, unsigned int const b)
+{
+    if (a == b)
+    {
+        return a;
+    }
+    else if (a > b)
+    {
+        return gcd_euclid2(a - b, b);
+    }
+    else
+    {
+        return gcd_euclid2(a, b - a);
+    }
+    
+}
+int main()
+{
+    std::cout << "Input numbers: ";
+    unsigned int a, b;
+    std::cin >> a >> b;
+
+    std::cout << "gcd1(" << a << "," << b << ")=" << gcd_euclid(a, b) << '\n';
+    std::cout << "gcd2(" << a << "," << b << ")=" << gcd_euclid2(a, b) << '\n';
+    return 0;
+}
