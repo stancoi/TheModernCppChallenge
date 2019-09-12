24113c8fccf4bb5505a9431aa533fc53eadf934e
stanco <stanco81@gmail.com>
2019-09-12 12:04:44 +0200

    Largest prime

---
+++ Chapter01/04/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+bool isPrime(unsigned int n)
+{
+    if (n <=1)
+        return false;
+
+    for (unsigned int i = 2; i < n; ++i)
+    {
+        if (n % i == 0)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+int main()
+{
+    unsigned int limit = 0;
+    std::cout << "Enter limit: ";
+    std::cin >> limit;
+
+    for (int i = limit; i > 1; i--)
+    {
+        if (isPrime(i))
+        {
+            std::cout << "Largest prime:" << i << '\n';
+            return 0;
+        }
+    }
+    return 0;
+}
