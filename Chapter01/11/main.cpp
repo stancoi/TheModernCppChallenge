364a65b4819f1a4d40b3f01c63da7b46ba490918
stanco <stanco81@gmail.com>
2019-09-24 14:12:51 +0200

    Include utility header for std::pair

--- Chapter01/11/main.cpp
+++ Chapter01/11/main.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <utility>
 
 static std::string to_roman(unsigned int value)
 {
-    std::vector<std::pair<unsigned int ,char const*>> roman
+    std::vector<std::pair<unsigned int, char const*>> roman
     {
-        {1000, "M"},{900, "CM"},
-        {500, "D"},{400, "CD"},
-        {100, "C"},{90, "XC"},
-        {50, "L"},{40, "XL"},
-        {10, "X"},{9, "IX"},
-        {5, "V"},{4, "IV"},
+        {1000, "M"}, {900, "CM"},
+        {500, "D"}, {400, "CD"},
+        {100, "C"}, {90, "XC"},
+        {50, "L"}, {40, "XL"},
+        {10, "X"}, {9, "IX"},
+        {5, "V"}, {4, "IV"},
         {1, "I"}
     };
 
