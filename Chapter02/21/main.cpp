0a98021ddcb104dc5a2464fa7900415e0eff115d
stancoi <stanco81@gmail.com>
2020-02-28 12:01:26 +0100

    Chapter02/21: Mocking functions for linux

--- Chapter02/21/main.cpp
+++ Chapter02/21/main.cpp
@@ -2,6 +2,30 @@
 #include <windows.h>
 #else
 using HANDLE = void*;
+constexpr unsigned long GENERIC_READ             = 0x80000000L;
+constexpr unsigned long GENERIC_WRITE            = 0x40000000L;
+constexpr unsigned long CREATE_NEW               = 1;
+constexpr unsigned long CREATE_ALWAYS            = 2;
+constexpr unsigned long OPEN_EXISTING            = 3;
+constexpr unsigned long OPEN_ALWAYS              = 4;
+constexpr unsigned long TRUNCATE_EXISTING        = 5;
+constexpr unsigned long FILE_SHARE_READ          = 1;
+constexpr unsigned long FILE_ATTRIBUTE_NORMAL    = 0x00000080;
+
+constexpr long long INVALID_HANDLE_VALUE = -1;
+
+int CloseHandle(HANDLE hObject) {return 0;}
+
+HANDLE CreateFileW(char const *, unsigned long, unsigned long, void*,
+                    unsigned long, unsigned long, HANDLE)
+{
+    return INVALID_HANDLE_VALUE;
+}
+
+int ReadFile(HANDLE, void*, unsigned long, unsigned long, void*)
+{
+    return 0;
+}
 #endif
 #include <iostream>
 #include <vector>
